4850a47f46bacb8e2515ee9b81abca1a8c45e6ef
Lawrence Lomax <lomax.lawrence@gmail.com>
2013-06-14 15:44:38 -0700

    Static lib

---
+++ LLSemanticLayout/LLSemanticLayout.m
@@ -0,0 +1,13 @@
+//
+//  LLSemanticLayout.m
+//  LLSemanticLayout
+//
+//  Created by Lawrence Lomax on 14/06/13.
+//  Copyright (c) 2013 Lawrence Lomax. All rights reserved.
+//
+
+#import "LLSemanticLayout.h"
+
+@implementation LLSemanticLayout
+
+@end
